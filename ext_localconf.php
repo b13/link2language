335050dc0a64416d700df54aaaba1f4ffde01bd8
denvis <dennis.panzner@googlemail.com>
2022-04-29 16:29:21 +0200

    fix uppercase in namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,8 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['removeParams_PostProc']['link2language'] = \B13\Link2language\RteParserTypoLinkHook::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['removeParams_PostProc']['link2language'] = \B13\Link2Language\RteParserTypoLinkHook::class;
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    'TCEMAIN.linkHandler.page.handler = B13\\Link2language\\LinkHandler\\PageLinkHandler'
+    'TCEMAIN.linkHandler.page.handler = B13\\Link2Language\\LinkHandler\\PageLinkHandler'
 );
